4faf2a7d545443a324e96c77c61a0823d9fa963d
Azriel Aprieliant <151019851+AzrielAprieliant@users.noreply.github.com>
2025-07-31 09:18:34 +0700

    pengajar_dashboard.py

--- pengajar_dashboard.py
+++ pengajar_dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="Dashboard Instruktur", layout="wide", initial_sidebar_state="collapsed")
+
 # Load data
 df = pd.read_excel("Penilaian Gabung dengan Nama Unit.xlsx")
 
